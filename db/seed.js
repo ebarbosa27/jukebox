f8d52aaf56568ba320e4c6e8f39a946adb74cc92
ebarbosa27 <ebarbosa5449@gmail.com>
2025-08-27 20:34:06 -0500

    Skeleton of a seeding function

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seed();
@@ -6,5 +7,27 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  const playlistList = [];
+  const trackList = [];
+  for (let i = 0; i < 15; i++) {
+    const playlist = {
+      name: faker.animal.petName(),
+      description: faker.person.bio,
+    };
+    playlistList.push(playlist);
+  }
+  for (let i = 0; i < 30; i++) {
+    const track = {
+      name: faker.music.songName(),
+      duration: faker.number.int({ min: 60000, max: 360000 }),
+    };
+  }
+
+  for (let i = 0; i < 15 * 30; i++) {
+    // 50/50 to add item to track
+    const random = Math.ceil(Math.random() * 2);
+    if (random === 2) continue;
+  }
+
+  console.log();
 }
